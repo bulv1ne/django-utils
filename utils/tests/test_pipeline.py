2695a1873f53b149136804e1f1dfee8b3a88544a
Niels Lemmens <draso.odin@gmail.com>
2017-04-06 15:30:13 +0200

    Don't output call_command

--- utils/tests/test_pipeline.py
+++ utils/tests/test_pipeline.py
@@ -1,3 +1,5 @@
+from io import StringIO
+
 from django.core.management import call_command
 from django.test import TestCase
 
@@ -5,5 +7,5 @@ from django.test import TestCase
 class PipelineTestCase(TestCase):
 
     def test_success(self):
-        call_command('collectstatic', '--noinput')
-        call_command('clean_staticfilesjson')
+        call_command('collectstatic', '--noinput', stdout=StringIO())
+        call_command('clean_staticfilesjson', stdout=StringIO())
