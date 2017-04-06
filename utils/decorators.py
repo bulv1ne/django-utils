ce191151fb9530fccf568042d0bd4fcf44ffd076
Niels Lemmens <draso.odin@gmail.com>
2017-04-06 16:04:25 +0200

    Fix cache_function and add test_decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,6 +1,20 @@
 from functools import wraps
+from itertools import chain
 
 from django.core.cache import cache
+
+
+def create_cache_key(func, args, kwargs):
+    return '-'.join(
+        map(
+            str,
+            chain(
+                [func.__qualname__],
+                args,
+                map(':'.join, kwargs.items()),
+            )
+        )
+    )
 
 
 class cache_function:
@@ -11,9 +25,11 @@ class cache_function:
     def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            key = self.key or func.__qualname__
-            if callable(key):
-                key = key(*args, **kwargs)
+            key = self.key
+            if key is None:
+                key = create_cache_key(func, args, kwargs)
+            elif callable(key):
+                key = create_cache_key(func, [key(*args, **kwargs)], {})
             data = cache.get(key)
             if data:
                 return data
