35575d8a7d998cf2a6b693fef305084baad8cf96
Niels Lemmens <draso.odin@gmail.com>
2017-04-07 13:49:06 +0200

    Add test_latency_middleware.py

--- utils/middlewares/latency.py
+++ utils/middlewares/latency.py
@@ -1,17 +1,16 @@
+import logging
 import time
-
-from utils.logging import logger
 
 
 class LatencyMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
+        self.logger = logging.getLogger(__name__)
 
     def __call__(self, request):
-        log = logger.name(__name__)
         start_time = time.time()
         try:
             return self.get_response(request)
         finally:
-            ms = int((time.time() - start_time) * 1000)
-            log.fields(url=request.build_absolute_uri()).info('Latency {}ms'.format(ms))
+            ds = int((time.time() - start_time) * 1000)
+            self.logger.info('url: {} Latency {}ms'.format(request.build_absolute_uri(), ds))
