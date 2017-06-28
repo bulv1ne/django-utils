5ac04091bad16927b00b8107f40359dfc7f7119a
Niels Lemmens <draso.odin@gmail.com>
2017-06-28 15:20:15 +0200

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ extras_require['all'] = list(chain(extras_require.values()))
 setup(
     name='bulv1ne-django-utils',
     packages=find_packages(exclude=['tests']),
-    version='0.1.2',
+    version='0.1.3',
     description='Django utils for all kinds of situations',
     author='Niels Lemmens',
     license='MIT',
