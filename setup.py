64533cc67987b1b3a0ec3342375a03bb87eaedeb
Niels Lemmens <draso.odin@gmail.com>
2021-01-12 11:04:29 +0100

    Bump version: 0.2.7 → 0.3.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 setup(
     name="bulv1ne-django-utils",
     packages=find_packages(exclude=["tests"]),
-    version="0.2.7",
+    version="0.3.0",
     description="Django utils for all kinds of situations",
     long_description=long_description,
     author="Niels Lemmens",
